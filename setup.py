71a1bc6f899581101e1d4347d71d6a21a42e5136
Tera Earlywine <dev@teraearlywine.com>
2024-06-26 10:05:59 -0700

    *

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/teraearlywine/analytics',
-    packages=find_packages("core"),
+    packages=find_packages("src"),
     include_package_data=True,
     # install_requires=[
     #     'dependency1',
