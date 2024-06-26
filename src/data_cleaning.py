ed5fb1cd4f2013873f7ee627887781f3047df202
Tera Earlywine <dev@teraearlywine.com>
2024-06-26 10:33:21 -0700

    *

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -13,6 +13,7 @@ import pandas as pd
 import os
 import uuid
 from datetime import datetime
+from skimpy import skim
 from core.schema import housing_schema
 
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
@@ -90,34 +91,19 @@ def started_annual_housing_units():
     )
     df_final = df_cleaned.dropna(subset=["Year"])
     df_final.reset_index(drop=True, inplace=True)
-    df_final.columns = [
-        "year",
-        "single_family_total",
-        "single_family_built_for_sale",
-        "single_family_fee_simple",
-        "single_family_contractor_built",
-        "single_family_owner_built",
-        "single_family_detached",
-        "single_family_attached",
-        "single_family_median_square_feet",
-        "single_family_average_square_feet",
-        "multi_family_total",
-        "multi_family_for_sale",
-        "multi_family_for_rent",
-        "multi_family_2_to_4_units",
-        "multi_family_5_to_9_units",
-        "multi_family_10_to_19_units",
-        "multi_family_20_or_more_units",
-        "multi_family_median_square_feet_per_unit",
-        "multi_family_average_square_feet_per_unit",
-    ]
+    df_final.columns = housing_schema # Imported
     non_numeric_years = df_final[~df_final["year"].astype(str).str.isnumeric()]
     df_final_cleaned = df_final.drop(non_numeric_years.index)
     df_final_cleaned["year"] = df_final_cleaned["year"].astype(int)
     return df_final_cleaned
 
+def skim_data(df):
+    return skim(df)
 
-# if __name__ == "__main__":
-
-#     started_annual_housing_units()
-#     completed_annual_housing_units()
+def completed_housing_final():
+    """
+    FINAL data cleaning, adding UUID & created_ts to dataframe 
+    """
+    data = completed_annual_housing_units()
+    df = add_unique_key_and_created_ts(data)
+    return df
