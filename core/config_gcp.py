b0c3fb006510c7c1ebb57234781790b1ba548b22
Tera Earlywine <dev@teraearlywine.com>
2024-06-26 13:08:43 -0700

    *

--- core/config_gcp.py
+++ core/config_gcp.py
@@ -1,18 +1,19 @@
+"""CONFIG GOOGLE CLOUD PROJECT 
+    
+Load Job Config attributes 
+https://cloud.google.com/bigquery/docs/reference/rest/v2/Job#JobConfigurationLoad.FIELDS.create_disposition
 
-
+        
+"""
 class GCP: 
     def __init__(self): 
         self.project_id = 'teradata-development'
         self.location = 'us-central1'
-
-        # Load Job Config attributes 
-        # https://cloud.google.com/bigquery/docs/reference/rest/v2/Job#JobConfigurationLoad.FIELDS.create_disposition
-        # create_disposition
         self.create_if_needed = 'CREATE_IF_NEEDED'
         self.create_never = 'CREATE_NEVER' # table must already exist
         self.write_truncate = 'WRITE_TRUNCATE'
         self.write_append = 'WRITE_APPEND'
         self.write_empty = 'WRITE_EMPTY'
-        
+
 if __name__ == "__main__":
     GCP()
