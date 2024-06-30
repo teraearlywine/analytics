2356b17f8d86358ad68cc045144df7defe2044dd
Tera Earlywine <dev@teraearlywine.com>
2024-06-30 11:27:59 -0700

    *

--- sql_transformations/models/pele_intermediate/pele_intermediate__annual_completed_housing.sql
+++ sql_transformations/models/pele_intermediate/pele_intermediate__annual_completed_housing.sql
@@ -15,22 +15,16 @@
 SELECT  uuid
       , created_ts
       , year
-      , STRUCT(
-            single_family_total
-          , single_family_built_for_sale
-          , single_family_fee_simple
-          , single_family_contractor_built
-          , single_family_owner_built
-      ) AS single_family
-      , STRUCT(
-            multi_family_total
-          , multi_family_for_sale
-          , multi_family_for_rent
-          , multi_family_2_to_4_units
-          , multi_family_5_to_9_units
-          , multi_family_10_to_19_units
-          , multi_family_20_or_more_units
-      ) AS multi_family
+      , single_family_total
+      , single_family_built_for_sale
+      , single_family_fee_simple
+      , single_family_contractor_built
+      , single_family_owner_built
+      , multi_family_total
+      , multi_family_for_sale
+      , multi_family_for_rent
+      , multi_family_2_to_4_units
+      , multi_family_5_to_9_units
+      , multi_family_10_to_19_units
+      , multi_family_20_or_more_units
 FROM  {{ ref('annual_completed_housing') }}
--- GROUP BY 
-      -- uuid, created_ts, year
