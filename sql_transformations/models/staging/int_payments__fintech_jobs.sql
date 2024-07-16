0e6def7a911e829785e9c9f87486ac729bb31396
Tera Earlywine <dev@teraearlywine.com>
2024-07-16 13:20:46 -0700

    *

---
+++ sql_transformations/models/staging/int_payments__fintech_jobs.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        materialized='table',
+        persist_docs={
+            "relation": true, 
+            "columns": true
+        }
+    )
+}}
+
+SELECT  job_id 
+      , job_title
+      , description 
+      , level
+      , department
+      , salary_budget
+FROM    {{ ref( 'fintech_jobs' )}}
