3b7e7131b5bb9fd6eca8c9dceed81465be65ff1d
Tera Earlywine <dev@teraearlywine.com>
2024-08-17 17:29:06 -0700

    *

--- sql_transformations/models/marts/dim_product.sql
+++ sql_transformations/models/marts/dim_product.sql
@@ -6,6 +6,7 @@
 }}
 
 SELECT  fk_product_stock_code_id AS pk_product_id 
+      , CURRENT_DATE AS created_dt -- TODO: scd?
       , description
       , unit_price
 FROM    {{ ref('stg__transaction_facts') }} -- treating as 'source' for the example 
