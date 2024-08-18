ec00a9b9fd21b0f9c687bd6295db6e7989e2b312
Tera Earlywine <dev@teraearlywine.com>
2024-08-17 17:28:30 -0700

    *

---
+++ sql_transformations/models/marts/dim_customers.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        schema='dim_dbt_tera'
+      , cluster_by = "pk_customer_id"
+    )
+}}
+
+SELECT  fk_customer_id AS pk_customer_id 
+      , CURRENT_DATE AS created_dt
+      , customer_country 
+      , '' AS city
+      , '' AS zip_code
+FROM    {{ ref('stg__transaction_facts') }} -- treating as 'source' for the example 
+GROUP BY 
+        pk_customer_id 
+      , customer_Country
+      , city
+      , zip_code
---
+++ sql_transformations/models/marts/dim_product.sql
@@ -0,0 +1,15 @@
+{{
+    config(
+        schema='dim_dbt_tera'
+      , cluster_by = "pk_product_id"
+    )
+}}
+
+SELECT  fk_product_stock_code_id AS pk_product_id 
+      , description
+      , unit_price
+FROM    {{ ref('stg__transaction_facts') }} -- treating as 'source' for the example 
+GROUP BY 
+        pk_product_id
+      , description
+      , unit_price
